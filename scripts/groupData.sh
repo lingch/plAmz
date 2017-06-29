754f96d4e57a27b97fbc4fa8f512ba4cda0bfaa8
lingch <cyberlingch@gmail.com>
2017-06-30 00:00:36 +0800

    505 ok

--- scripts/groupData.sh
+++ scripts/groupData.sh
@@ -10,7 +10,28 @@ CODE=$1;
 rm -rf "$CODE-all"
 rm -f "$CODE-all.csv" 
 mkdir "$CODE-all"
-find "$CODE/" -name "*.tbi" | xargs -I '{}' ln '{}' "$CODE-all/"
+
+TMPFILE="/tmp/tbi"
+
+find "$CODE/" -name "*.tbi" > $TMPFILE
+
+while read line; do
+	NAME=`echo $line | rev | cut -d/ -f1| rev`
+	if [ ! -e "$CODE-all/$NAME" ]; then
+		ln $line "$CODE-all/"
+	fi
+done <$TMPFILE;
+
 cat template/template-header.csv > "$CODE-all.csv"
 find "$CODE/" -name "*.csv" | xargs -I '{}' cat '{}' >> "$CODE-all.csv"
 
+
+ls -1 $CODE-all/*.tbi | cut -d/ -f2 | cut -d. -f1> $TMPFILE
+i=1;
+while read line; do
+	mv "$CODE-all/$line.tbi" "$CODE-all/$i.tbi"
+	sed -i "s/$line/$i/g" "$CODE-all.csv"
+	i=`expr $i + 1`
+done <$TMPFILE
+
+#rm $TMPFILE
