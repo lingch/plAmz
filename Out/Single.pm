aae1a3c788c3a8a54499685268611eb32ad08648
lingch <cyberlingch@gmail.com>
2017-07-02 13:06:16 +0800

    move extration method to Out directory

---
+++ Out/Single.pm
@@ -0,0 +1,162 @@
+package Out::Single;
+
+use Util;
+use TBCsv;
+use File::Path qw(make_path remove_tree);
+use Digest::MD5 qw(md5_hex);
+use POSIX;
+
+
+sub new {
+	my $class = shift;
+	my $code = shift;
+
+	$class = ref $class if ref $class;
+	my $self = bless {
+
+		code=>$code
+		}, $class;
+	
+	return $self;
+}
+
+sub arr_count{
+	my $a1 = shift;
+	my $a2 = shift;
+
+	return scalar(@{$a1}) + scalar(@{$a2});
+}
+
+sub check_merge{
+	my $jo = shift;
+	my $marked = shift;
+	my $toMerge = shift;
+
+	return 0 if length($marked) > 10;
+
+	if(arr_count($jo->{$marked},$jo->{$toMerge}) <= 24){
+		return 1;
+	} else{
+		return 0;
+	}
+}
+
+sub extra{
+	my $self = shift;
+	my $items = shift;
+
+	my $itemsTree = $self->transform($items);
+
+	for my $color (sort keys %{$itemsTree}){
+		for my $price (sort keys %{$itemsTree->{$color}}){
+			$itemsTree->{$color}->{$price} = merge_w($itemsTree->{$color}->{$price});
+		}
+		$self->genDataPack($itemsTree->{$color},$color);
+	}
+}
+
+sub merge_w{
+	my $jo = shift;
+
+	my $priceHash={};
+	my $mergePoint = undef;
+	for my $w (sort keys %{$jo}){
+		unless (defined $mergePoint) {
+			$mergePoint = $w;
+			next;
+		}
+
+		my $n = scalar(@{$jo->{$w}});
+		if($n > 24){
+			#split
+			my $piece = ceil($n / 24);
+			my $step = ceil($n / $piece);
+			my $i = 0;
+			for ($i = 0; $i < $piece-1; $i++) {
+				$jo->{"w_$i"} = $jo->{$w}->[$i*$step..($i+1)*$step-1];
+			}
+			$jo->{"w_$i"} = $jo->{$w}->[$i*$step..-1];
+			delete $jo->{$w};
+		}else{
+			
+
+			if(! check_merge($jo,$mergePoint,$w)){
+				$mergePoint = $w;
+				next;
+			}
+
+			my $newkey = "$mergePoint";
+			$newkey .= " $w" if index($mergePoint,$w) < 0;
+			my $tmp = [ @{$jo->{$mergePoint}},@{$jo->{$w}}];	#	in case $newkey eq $mergePoint
+			delete $jo->{$mergePoint};
+			delete $jo->{$w};
+			$jo->{$newkey} = $tmp;
+			$mergePoint = $newkey;
+		}
+	}
+
+	return $jo;
+}
+
+sub genDataPack{
+
+	my $self = shift;
+	our ( $jo, $color) = @_;
+	my $prefix = $self->{code};
+	my $color_p = Util::normalizePath($color);
+
+	# $jo_color_w->{hello} = "world";
+	my $csv = TBCsv->new();
+	for our $price (keys %{$jo}){
+		our $jo_price = $jo->{$price};
+
+		make_path( "$prefix/$color_p/$price");
+
+		for my $w (keys %{$jo_price}){
+			my $w_p = Util::normalizePath($w);
+			make_path( "$prefix/$color_p/$price/$w_p/");
+
+			#transfer imgs_local to t_picture
+			my $item_0 = $jo_price->{$w}->[0];
+			my $t_picture = "";
+			#skip those no picture?
+			next unless defined $item_0->{imgs_local} and scalar(@{$item_0->{imgs_local}})>0;
+			for (my $i = 0; $i < scalar(@{$item_0->{imgs_local}}); $i++) {
+				my $hash = md5_hex($item_0->{imgs_remote}->[$i]);
+				$t_picture .= "$hash:1:$i:|;";
+			    link $item_0->{imgs_local}->[$i],"$prefix/$color_p/$price/$w_p/$hash.tbi";
+			    $item_0->{imgs_local}->[$i] = "$hash";
+			}
+			$jo_price->{$w}->[0]->{t_picture} = $t_picture;
+
+			my $lines = $csv->stringify($jo_price->{$w},$w);
+
+			Util::writeFile($lines,"$prefix/$color_p/$price/$w_p.csv");
+		}
+	}
+}
+
+sub transform{
+	my $self = shift;
+	my $items = shift;
+
+	my $jo = {};
+	for my $item ( @{$items}){
+		# my $title = $item->{title_cn} or next;
+		my $color = $item->{color} or next;
+		my $size = $item->{size} or next;
+		my $price = $item->{t_price} = ceil($item->{price} * 7.0) or next;
+		my ($w) = split(/ /, $size);
+		# my $w = $item->{w} or next;
+		# my $l = $item->{l} or next;
+
+		push @{$jo->{$color}->{$price}->{$w}}, $item;
+	}
+
+	return $jo;
+}
+
+
+1;
+
+
--- app.pl
+++ app.pl
@@ -17,7 +17,7 @@ use File::Path qw(make_path remove_tree);
 use Util;
 use MyDownloader;
 use DBStore;
-use TBCsv;
+use Out::Single;
 
 require "html_parser.pl";
 my $ROOT={
@@ -152,62 +152,11 @@ sub extraData{
 	my $self = shift;
 
 	my $items = $self->{store}->getItemsAll();
+	my $out = Out::Single->new($self->{code});
 
-	my $itemsTree = $self->transform2Tree($items);
-
-	for my $color (sort keys %{$itemsTree}){
-		for my $price (sort keys %{$itemsTree->{$color}}){
-			$itemsTree->{$color}->{$price} = merge_w($itemsTree->{$color}->{$price});
-		}
-		$self->genDataPack($itemsTree->{$color},$color);
-	}
+	$out->extra($items);
 }
 
-sub genDataPack{
-
-	my $self = shift;
-	our ( $jo, $color) = @_;
-	my $prefix = $self->{code};
-	my $color_p = Util::normalizePath($color);
-
-	# $jo_color_w->{hello} = "world";
-	my $csv = TBCsv->new();
-	for our $price (keys %{$jo}){
-		our $jo_price = $jo->{$price};
-
-		make_path( "$prefix/$color_p/$price");
-
-		for my $w (keys %{$jo_price}){
-			my $w_p = Util::normalizePath($w);
-			make_path( "$prefix/$color_p/$price/$w_p/");
-
-			#transfer imgs_local to t_picture
-			my $item_0 = $jo_price->{$w}->[0];
-			my $t_picture = "";
-			#skip those no picture?
-			next unless defined $item_0->{imgs_local} and scalar(@{$item_0->{imgs_local}})>0;
-			for (my $i = 0; $i < scalar(@{$item_0->{imgs_local}}); $i++) {
-				my $hash = md5_hex($item_0->{imgs_remote}->[$i]);
-				$t_picture .= "$hash:1:$i:|;";
-			    link $item_0->{imgs_local}->[$i],"$prefix/$color_p/$price/$w_p/$hash.tbi";
-			    $item_0->{imgs_local}->[$i] = "$hash";
-			}
-			$jo_price->{$w}->[0]->{t_picture} = $t_picture;
-
-			my $lines = $csv->stringify($jo_price->{$w},$w);
-
-			Util::writeFile($lines,"$prefix/$color_p/$price/$w_p.csv");
-		}
-
-		# open F,"<:utf8",$temp_filename or die "cannot open template file $temp_filename";
-		# my $template = Text::Template->new(TYPE => 'FILEHANDLE', SOURCE=> \*F)
-		# 	or die "Couldn't construct template: $Text::Template::ERROR";
-
-		# my $result = $template->fill_in() or die $Text::Template::ERROR;
-
-		# Util::writeFile($result,"$prefix/$color_p/$price.csv");
-	}
-}
 
 sub downloadImgs{
 	my $self = shift;
@@ -300,131 +249,6 @@ sub handle_size{
 	return $jo;
 }
 
-
-sub arr_count{
-	my $a1 = shift;
-	my $a2 = shift;
-
-	return scalar(@{$a1}) + scalar(@{$a2});
-}
-
-sub check_merge{
-	my $jo = shift;
-	my $marked = shift;
-	my $toMerge = shift;
-
-	return 0 if length($marked) > 10;
-
-	if(arr_count($jo->{$marked},$jo->{$toMerge}) <= 24){
-		return 1;
-	} else{
-		return 0;
-	}
-}
-
-# sub splitByPrice{
-# 	my $w = shift;
-# 	my $from = 0;
-# 	my $ret = [];
-# 	for (my $i=0;$i<scalar(@{$w});$++){
-# 		next if($w->[$i]->{t_price} == $w->[$from]->{t_price});
-# 		push @{$ret}, $w->[$from..$i]; 
-# 	}
-
-# 	return $ret;
-# }
-
-# sub merge_l {
-# 	my $jo = shift;
-# 	my $ls = shift;
-
-# 	my $marked = undef;
-# 	for my $l (@{$ls}){
-# 		unless(defined $marked){
-# 			$marked = $current;
-# 			next;
-# 		}
-
-# 		if(check_merge($jo,$marked,$current)){
-# 			#merge
-# 			my $newkey .= "/$current";
-# 			$jo->{$newkey} = [ @{$jo->{$marked}},@{$jo->{$current}}];
-# 			delete $jo->{$marked};
-# 			delete $jo->{$current};
-# 			$marked = $newkey;
-# 			next;
-# 		}else{
-# 			#set new $marked
-# 			$marked = $current;
-# 		}	
-# 	}
-# }
-sub merge_w{
-	my $jo = shift;
-
-	my $priceHash={};
-	my $mergePoint = undef;
-	for my $w (sort keys %{$jo}){
-		unless (defined $mergePoint) {
-			$mergePoint = $w;
-			next;
-		}
-
-		my $n = scalar(@{$jo->{$w}});
-		if($n > 24){
-			#split
-			my $piece = ceil($n / 24);
-			my $step = ceil($n / $piece);
-			my $i = 0;
-			for ($i = 0; $i < $piece-1; $i++) {
-				$jo->{"w_$i"} = $jo->{$w}->[$i*$step..($i+1)*$step-1];
-			}
-			$jo->{"w_$i"} = $jo->{$w}->[$i*$step..-1];
-			delete $jo->{$w};
-		}else{
-			
-
-			if(! check_merge($jo,$mergePoint,$w)){
-				$mergePoint = $w;
-				next;
-			}
-
-			my $newkey = "$mergePoint";
-			$newkey .= " $w" if index($mergePoint,$w) < 0;
-			my $tmp = [ @{$jo->{$mergePoint}},@{$jo->{$w}}];	#	in case $newkey eq $mergePoint
-			delete $jo->{$mergePoint};
-			delete $jo->{$w};
-			$jo->{$newkey} = $tmp;
-			$mergePoint = $newkey;
-		}
-	}
-
-	return $jo;
-}
-
-sub split_w{
-	my $jo = shift;
-
-	for my $wr (sort keys %{$jo}){
-		my $price_hash = {};
-		for (my $i=0;$i<scalar(@{$jo->{$wr}});$i++){
-			my $price = $jo->{$wr}->[$i]->{t_price};
-			if(defined $price_hash->{$price}){
-				push @{$price_hash->{$price}}, $jo->{$wr}->[$i];
-			}else{
-				$price_hash->{$price} = [ $jo->{$wr}->[$i] ];
-			}
-		}
-		delete $jo->{$wr};
-
-		my $i=1;
-		for my $arr ( values %{$price_hash}){
-			$jo->{"$wr-$i"} = $arr;
-			$i++;
-		}
-	}
-	return $jo;
-}
 sub transform2Flat{
 	my $jo_asin = shift;
 
@@ -450,38 +274,7 @@ sub transform2Flat{
 	return $jo;
 }
 
-# sub findPriceSection {
-	
-# 	for my $p (sort keys %{$jo->{$color}}){
-# 		my ($low,$high) = split(/-/,$p);
-# 		$high = $low if undef $high;
-# 		return $p if $price >=$low and $price < $price*1.1
-# 	}
-# }
 
-sub transform2Tree{
-	my $self = shift;
-	my $items = shift;
-
-	my $jo = {};
-	for my $item ( @{$items}){
-		# my $title = $item->{title_cn} or next;
-		my $color = $item->{color} or next;
-		my $size = $item->{size} or next;
-		my $price = $item->{t_price} = ceil($item->{price} * 7.0) or next;
-		my ($w) = split(/ /, $size);
-		# my $w = $item->{w} or next;
-		# my $l = $item->{l} or next;
-
-		$jo->{$color} = {} unless defined $jo->{$color};
-		$jo->{$color}->{$price} = {} unless defined $jo->{$color}->{$price};
-		$jo->{$color}->{$price}->{$w} = [] unless defined $jo->{$color}->{$price}->{$w};
-
-		push @{$jo->{$color}->{$price}->{$w}}, $item;
-	}
-
-	return $jo;
-}
 
 1;
 
